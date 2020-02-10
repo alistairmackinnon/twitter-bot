63a8469913191384ab25e38bd1837da926515279
Ali <alistair.mackinnon@outlook.com>
2020-02-10 21:44:23 +0000

    reaturning search

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ auth = tweepy.OAuthHandler(config.consumer_token, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
-status = "Testing!"
-api.update_status(status=status)
+# shs
+query = '#scotfail'
+language = 'en'
+
+results = api.search(q=query, lang=language, tweet_mode="extended", count=100)
+
+for result in results:
+    if not hasattr(result, "retweeted_status"):
+        print(result.full_text)
