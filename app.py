05b15e552744a4394707421ed60344e9d35150ac
Ali <alistair.mackinnon@outlook.com>
2020-02-10 23:06:33 +0000

    adding hermes

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import tweepy
 import config
+from datetime import datetime, timedelta
 
 auth = tweepy.OAuthHandler(config.consumer_token, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
-# shs
-query = '#scotrail'
+
+def get_tweets(q, lang):
+    return api.search(q=q, lang=lang, tweet_mode="extended", count=100)
+
+
+time_diff = timedelta(hours=24)
+curr_date_time = datetime.now()
+
+query = '@hermesparcels missing'
 language = 'en'
 
-results = api.search(q=query, lang=language, tweet_mode="extended", count=100)
+results = get_tweets(query, language)
 
 for result in results:
     if not hasattr(result, "retweeted_status"):
-        print(result.user.screen_name)
-        print(result.full_text)
-        print(result.favorite_count)
-
+        if result.created_at >= curr_date_time - time_diff:
+            print(result.created_at)
+            print(result.user.screen_name)
+            print(result.full_text)
+            print(result.favorite_count)
