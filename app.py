0e423cd5b2db1bcad96c73b71536aee75716ad17
Ali <alistair.mackinnon@outlook.com>
2020-02-10 22:16:37 +0000

    adding attributes

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
 # shs
-query = '#scotfail'
+query = '#scotrail'
 language = 'en'
 
 results = api.search(q=query, lang=language, tweet_mode="extended", count=100)
 
 for result in results:
     if not hasattr(result, "retweeted_status"):
+        print(result.user.screen_name)
         print(result.full_text)
+        print(result.favorite_count)
+
